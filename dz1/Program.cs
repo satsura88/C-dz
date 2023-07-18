d5a32b58d8714afe5346438a91107a934507e6b1
satsura88 <satsura88@gmail.com>
2023-07-18 15:45:47 +0300

    end 2.0)

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -2,7 +2,7 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
-
+*/
 
 int max = 0;
 int min = 0;
@@ -24,7 +24,7 @@ else
     min = firstNumber;
 }
 Console.WriteLine("max = " + max + " и " + "min = " + min);
-*/
+
 /*
 Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 2, 3, 7 -> 7
@@ -57,3 +57,44 @@ if(thirdNumber > max)
 }
 
 Console.WriteLine("max = " + max);
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
